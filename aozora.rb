483c971a2a6fb6be4487a045ca83a602eeee3f70
ongaeshi <ongaeshi0621@gmail.com>
2014-01-27 01:16:23 +0900

    Add aozora.rb
    Don't move in a compatibility issue

---
+++ aozora.rb
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# require_relative './grn_array'
+require 'grn_mini'
+require 'find'
+require 'kconv'
+
+def read_file(filename)
+  text = File.read(filename)
+  Kconv.kconv(text, Kconv::UTF8).gsub("\r\n", "\n")
+end
+
+array = GrnMini::Array.new("db/aozora2.db")
+
+if array.empty?
+  # Input
+  if ARGV.empty?
+    puts "aozora.rb [input_dir]"
+    exit
+  end
+
+  Find.find(File.expand_path ARGV[0])  do |filename|
+    if File.file? filename
+      array << {filename: filename, text: read_file(filename)}
+    end
+    puts "Input : #{array.size}" if array.size > 0 && array.size % 100 == 0
+  end
+  puts "Input complete : #{array.size} files"
+
+else
+  # Search
+  unless ARGV.empty?
+    results = array.select(ARGV.join(" "))
+    puts "#{results.size} matches"
+    snippet = GrnMini::Util::text_snippet_from_selection_results(results)
+
+    results.each do |record|
+      puts "--- #{record.filename} ---"
+      snippet.execute(record.text).each do |segment|
+        puts segment.gsub("\n", "")
+      end
+    end
+  end
+end
+
