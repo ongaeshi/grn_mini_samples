184c51e92d2b3ad25121abb38cd087a7f967fa7c
ongaeshi <ongaeshi0621@gmail.com>
2014-01-28 01:13:02 +0900

    Add Ratings table

--- restaurants.rb
+++ restaurants.rb
@@ -9,9 +9,11 @@ require 'grn_mini'
 require 'csv'
 
 is_create = GrnMini::create_or_open("restaurants.db")
-restaurants = GrnMini::Array.new("Restaurants")
+restaurants = GrnMini::Hash.new("Restaurants")
+ratings     = GrnMini::Hash.new("Ratings")
 
-if is_create
+# if is_create
+if restaurants.size == 0 && ratings.size == 0
   restaurants.setup_columns(name:                   "レストラン",
                             property:               "",
                             alphabet:               "RESTAURANT",
@@ -34,8 +36,19 @@ if is_create
                             # closed:                 false,
                             )
 
+  ratings.setup_columns(restaurant_id:    restaurants,
+                        total:            0,
+                        food:             0,
+                        service:          0,
+                        atmosphere:       0,
+                        cost_performance: 0,
+                        title:            "タイトル",
+                        body:             "コメント",
+                        created_on:       Time.now,
+                        )
+
   CSV.foreach(File.join(ARGV[0], 'restaurants.csv'), headers: true) do |row|
-    restaurants << {
+    restaurants[row[0]] = {
       name:                   row[1],
       property:               row[2],
       alphabet:               row[3],
@@ -58,11 +71,40 @@ if is_create
       # closed:                 false,
     }
   end
+
+  CSV.foreach(File.join(ARGV[0], 'ratings.csv'), headers: true) do |row|
+    ratings[row[0]] = {
+      restaurant_id:    row[1],
+      # total:            row[3],
+      # food:             row[4],
+      # service:          row[5],
+      # atmosphere:       row[6],
+      # cost_performance: row[7],
+      title:            row[8],
+      body:             row[9],
+      # created_on:       row[11],
+    }
+  end
+  
 end
 
-p restaurants[1].attributes
+# p restaurants['310595'].attributes
+# p restaurants['10237'].attributes
+
+# p ratings['66111'].attributes
+
+# p restaurants[1].attributes
+# p restaurants[4].attributes
+# p restaurants[2583].attributes
+# p restaurants[2583-1].attributes
+# p restaurants[156445].attributes
+# p restaurants[156445-1].attributes
 
 restaurants.select("address:@新宿 name:@ラーメン name:@九州").each do |record|
+  p record.attributes
+end
+
+ratings.select("body:@びっくり body:@ラーメン body:@味噌 body:@醤油").each do |record|
   p record.attributes
 end
 
